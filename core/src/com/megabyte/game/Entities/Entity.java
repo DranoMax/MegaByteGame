14480a76e16fec61429146d10cc3cd7312d0a5bb
DranoMax <alex.scroggins@live.com>
2015-08-21 22:12:54 -0600

    Added Update method to Entity

--- core/src/com/megabyte/game/Entities/Entity.java
+++ core/src/com/megabyte/game/Entities/Entity.java
@@ -1,12 +1,18 @@
 package com.megabyte.game.Entities;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by ascroggins on 8/21/2015.
  */
 public abstract class Entity {
     private Sprite sprite;
+    private Vector2 acceleration = new Vector2(0,0);
+    private Vector2 velocity = new Vector2(0,0);
+
+    public void update(float delta) {
+    }
 
     public float getX() {
         return this.sprite.getX();
@@ -24,11 +30,35 @@ public abstract class Entity {
         this.sprite.setY(y);
     }
 
+    public Vector2 getPosition() {
+        return new Vector2(this.getX(), this.getY());
+    }
+
+    public void setPosition(Vector2 position) {
+        this.sprite.setPosition(position.x, position.y);
+    }
+
     public Sprite getSprite() {
         return sprite;
     }
 
     public void setSprite(Sprite sprite) {
         this.sprite = sprite;
+    }
+
+    public Vector2 getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(Vector2 acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public Vector2 getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector2 velocity) {
+        this.velocity = velocity;
     }
 }
