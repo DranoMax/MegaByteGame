4f7fc698ee980b4f24601764108f23631ce2824a
DranoMax <alex.scroggins@live.com>
2015-08-22 00:50:18 -0600

    Added Level class + more simplified code
    Updated to be more like the example I found at:
    http://www.javacodegeeks.com/2013/03/android-game-development-with-libgdx-collision-detection-part-4.html
    
    NOTE: Now the PC no longer stops falling at the bottom of the screen -
    now we need to add blocks for the PC to stand on.

--- core/src/com/megabyte/game/Model/PlayerCharacter.java
+++ core/src/com/megabyte/game/Model/PlayerCharacter.java
@@ -3,6 +3,9 @@ package com.megabyte.game.Model;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Pool;
 
 /**
  * Created by ascroggins on 8/21/2015.
@@ -15,6 +18,8 @@ public class PlayerCharacter extends Entity{
     public enum State {
         IDLE, WALKING, JUMPING, DYING
     }
+    public static final float SIZE = 0.5f; // half a unit
+
     private State state = State.IDLE;
     boolean	facingLeft = true;
     private static final long LONG_JUMP_PRESS 	= 150l;
@@ -23,11 +28,24 @@ public class PlayerCharacter extends Entity{
     private static final float MAX_JUMP_SPEED	= 7f;
     private static final float DAMP 			= 0.90f;
     private static final float MAX_VEL 			= 10f;
-
+    private boolean grounded = false;
     // these are temporary
     private static final float WIDTH = 10f;
     private long	jumpPressedTime;
     private boolean jumpingPressed;
+    private World world;
+
+    // Blocks that Bob can collide with any given frame
+    private Array<Block> collidable = new Array<Block>();
+
+    // This is the rectangle pool used in collision detection
+    // Good to avoid instantiation each frame
+    private Pool<Rectangle> rectPool = new Pool<Rectangle>() {
+        @Override
+        protected Rectangle newObject() {
+            return new Rectangle();
+        }
+    };
 
 
     public void jumpReleased() {
@@ -35,47 +53,48 @@ public class PlayerCharacter extends Entity{
         jumpingPressed = false;
     }
 
+    public PlayerCharacter(Sprite sprite, World world) {
+        this.setSprite(sprite);
+        this.getBounds().x = this.getPosition().x;
+        this.getBounds().y = this.getPosition().y;
+        this.getBounds().height = SIZE;
+        this.getBounds().width = SIZE;
+
+        this.world = world;
+    }
+
     @Override
     public void update(float delta) {
         processInput();
 
+        // If this is grounded then reset the state to IDLE 
+        if (grounded && this.getState().equals(State.JUMPING)) {
+            this.setState(State.IDLE);
+        }
+
+        // Setting initial vertical acceleration 
         this.getAcceleration().y = GRAVITY;
+
+        // Convert acceleration to frame time
         this.getAcceleration().scl(delta);
+
+        // apply acceleration to change velocity
         this.getVelocity().add(this.getAcceleration().x, this.getAcceleration().y);
-        if (this.getAcceleration().x == 0) this.getVelocity().x *= DAMP;
+
+        // checking collisions with the surrounding blocks depending on this's velocity
+        checkCollisionWithBlocks(delta);
+
+        // apply damping to halt this nicely 
+        this.getVelocity().x *= DAMP;
+
+        // ensure terminal velocity is not exceeded
         if (this.getVelocity().x > MAX_VEL) {
             this.getVelocity().x = MAX_VEL;
         }
         if (this.getVelocity().x < -MAX_VEL) {
             this.getVelocity().x = -MAX_VEL;
         }
-
-        // Get Keyboard updates
-        this.updatePosition();
-
-        // Don't fall through the floor!
-        if (this.getPosition().y < 0) {
-            this.setY(0f);
-            if (this.getState().equals(State.JUMPING)) {
-                this.setState(State.IDLE);
-            }
-        }
-        // Don't run through the left wall!
-        if (this.getPosition().x < 0) {
-            this.getPosition().x = 0;
-            this.setPosition(this.getPosition());
-            if (!this.getState().equals(State.JUMPING)) {
-                this.setState(State.IDLE);
-            }
-        }
-        // Don't run through the right wall!
-        if (this.getPosition().x > WIDTH ) {
-            this.getPosition().x = WIDTH ;
-            this.setPosition(this.getPosition());
-            if (!this.getState().equals(State.JUMPING)) {
-                this.setState(State.IDLE);
-            }
-        }
+        updatePosition();
     }
 
     /** Change this's state and parameters based on input controls **/
@@ -120,15 +139,104 @@ public class PlayerCharacter extends Entity{
         return false;
     }
 
-    public PlayerCharacter(Sprite sprite) {
-        this.setSprite(sprite);
+    /** Collision checking **/
+    private void checkCollisionWithBlocks(float delta) {
+        // scale velocity to frame units 
+        this.getVelocity().scl(delta);
+
+        // Obtain the rectangle from the pool instead of instantiating it
+        Rectangle bobRect = rectPool.obtain();
+        // set the rectangle to bob's bounding box
+        bobRect.set(this.getBounds().x, this.getBounds().y, this.getBounds().width, this.getBounds().height);
+
+        // we first check the movement on the horizontal X axis
+        int startX, endX;
+        int startY = (int) this.getBounds().y;
+        int endY = (int) (this.getBounds().y + this.getBounds().height);
+        // if Bob is heading left then we check if he collides with the block on his left
+        // we check the block on his right otherwise
+        if (this.getVelocity().x < 0) {
+            startX = endX = (int) Math.floor(this.getBounds().x + this.getVelocity().x);
+        } else {
+            startX = endX = (int) Math.floor(this.getBounds().x + this.getBounds().width + this.getVelocity().x);
+        }
+
+        // get the block(s) bob can collide with
+        populateCollidableBlocks(startX, startY, endX, endY);
+
+        // simulate bob's movement on the X
+        bobRect.x += this.getVelocity().x;
+
+        // clear collision boxes in world
+        world.getCollisionRects().clear();
+
+        // if bob collides, make his horizontal velocity 0
+        for (Block block : collidable) {
+            if (block == null) continue;
+            if (bobRect.overlaps(block.getBounds())) {
+                this.getVelocity().x = 0;
+                world.getCollisionRects().add(block.getBounds());
+                break;
+            }
+        }
+
+        // reset the x position of the collision box
+        bobRect.x = this.getPosition().x;
+
+        // the same thing but on the vertical Y axis
+        startX = (int) this.getBounds().x;
+        endX = (int) (this.getBounds().x + this.getBounds().width);
+        if (this.getVelocity().y < 0) {
+            startY = endY = (int) Math.floor(this.getBounds().y + this.getVelocity().y);
+        } else {
+            startY = endY = (int) Math.floor(this.getBounds().y + this.getBounds().height + this.getVelocity().y);
+        }
+
+        populateCollidableBlocks(startX, startY, endX, endY);
+
+        bobRect.y += this.getVelocity().y;
+
+        for (Block block : collidable) {
+            if (block == null) continue;
+            if (bobRect.overlaps(block.getBounds())) {
+                if (this.getVelocity().y < 0) {
+                    grounded = true;
+                }
+                this.getVelocity().y = 0;
+                world.getCollisionRects().add(block.getBounds());
+                break;
+            }
+        }
+        // reset the collision box's position on Y
+        bobRect.y = this.getPosition().y;
+
+        // update Bob's position
+        this.getPosition().add(this.getVelocity());
+        this.getBounds().x = this.getPosition().x;
+        this.getBounds().y = this.getPosition().y;
+
+        // un-scale velocity (not in frame time)
+        this.getVelocity().scl(1 / delta);
+
     }
 
+    /** populate the collidable array with the blocks found in the enclosing coordinates **/
+    private void populateCollidableBlocks(int startX, int startY, int endX, int endY) {
+        collidable.clear();
+        for (int x = startX; x <= endX; x++) {
+            for (int y = startY; y <= endY; y++) {
+                if (x >= 0 && x < world.getLevel().getWidth() && y >=0 && y < world.getLevel().getHeight()) {
+                   collidable.add(world.getLevel().get(x, y));
+                }
+            }
+        }
+    }
 
     private void updatePosition() {
             this.setPosition(this.getPosition().add(this.getVelocity()));
-//            bounds.x = position.x;
-//            bounds.y = position.y;
+            this.getBounds().x = this.getPosition().x;
+            this.getBounds().y = this.getPosition().y;
+        System.out.println(this.getPosition());
 //            stateTime += delta;
     }
 
--- core/src/com/megabyte/game/Model/World.java
+++ core/src/com/megabyte/game/Model/World.java
@@ -3,61 +3,106 @@ package com.megabyte.game.Model;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class World {
 
+    private static int WIDTH;
+    private static int HEIGHT;
+
     /** The blocks making up the world **/
-    Array<Block> blocks = new Array<Block>();
+    private Array<Block> blocks = new Array<Block>();
     /** Our player controlled hero **/
-    PlayerCharacter playerCharacter;
-    Mom mom;
+    private PlayerCharacter playerCharacter;
+    private Mom mom;
+    /** A world has a level through which Bob needs to go through **/
+    Level level;
+
+    /** The collision boxes **/
+    Array<Rectangle> collisionRects = new Array<Rectangle>();
 
     // Getters -----------
-    public Array<Block> getBlocks() {
-        return blocks;
+
+    public Array<Rectangle> getCollisionRects() {
+        return collisionRects;
     }
+
+    // Getters -----------
     public PlayerCharacter getPlayerCharacter() {
         return playerCharacter;
     }
     public Mom getMom() {
         return mom;
     }
+    public Level getLevel() {
+        return level;
+    }
     // --------------------
 
-    public World() {
+    public World(int width, int height) {
+        WIDTH = width;
+        HEIGHT = height;
         createDemoWorld();
     }
 
     // Sets up all the graphics + spriteBatch for drawing sprites
     private void setupEntities() {
-        playerCharacter = new PlayerCharacter(new Sprite(new Texture("badlogic.jpg")));
+        playerCharacter = new PlayerCharacter(new Sprite(new Texture("badlogic.jpg")), this);
         mom = new Mom();
     }
 
     private void createDemoWorld() {
         setupEntities();
+        level = new Level();
+    }
 
-        for (int i = 0; i < 10; i++) {
-            blocks.add(new Block(new Vector2(i, 0)));
-            blocks.add(new Block(new Vector2(i, 6)));
-            if (i > 2)
-                blocks.add(new Block(new Vector2(i, 1)));
+    /** Return only the blocks that need to be drawn **/
+    public List<Block> getDrawableBlocks(int width, int height) {
+        int x = (int)playerCharacter.getPosition().x - width;
+        int y = (int)playerCharacter.getPosition().y - height;
+        if (x < 0) {
+            x = 0;
         }
-        blocks.add(new Block(new Vector2(9, 2)));
-        blocks.add(new Block(new Vector2(9, 3)));
-        blocks.add(new Block(new Vector2(9, 4)));
-        blocks.add(new Block(new Vector2(9, 5)));
+        if (y < 0) {
+            y = 0;
+        }
+        int x2 = x + 2 * width;
+        int y2 = y + 2 * height;
+        if (x2 > 800) {
+            x2 = 800 - 1;
+        }
+        if (y2 > 480) {
+            y2 = 480 - 1;
+        }
 
-        blocks.add(new Block(new Vector2(6, 3)));
-        blocks.add(new Block(new Vector2(6, 4)));
-        blocks.add(new Block(new Vector2(6, 5)));
+        List<Block> blocks = new ArrayList<Block>();
+        Block block;
+//        for (int col = x; col <= x2; col++) {
+//            for (int row = y; row <= y2; row++) {
+//                block = level.getBlocks()[col][row];
+//                if (block != null) {
+//                    blocks.add(block);
+//                }
+//            }
+//        }
+        return blocks;
     }
 
     public void update() {
         // Listen for keyboard events so that we move our PC around
         playerCharacter.update(Gdx.graphics.getDeltaTime());
         mom.update(Gdx.graphics.getDeltaTime());
+    }
+
+    public static int getWidth() {
+        return WIDTH;
+    }
+
+    public static int getHeight() {
+        return HEIGHT;
     }
 }
